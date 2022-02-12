b94f7195e3e6d9f53ef6176fee310d2d8dc61569
A1ic3-g <psyag8@exmail.nottingham.ac.uk>
2022-02-12 21:13:15 +0000

    work on pipeline

--- src/stitching.py
+++ src/stitching.py
@@ -1,8 +1,13 @@
-from PIL import image
+from PIL import Image
 
-def create_canvas(size_of_image=64, grid_width=10, grid_height=10):
-    pass
+def create_canvas(size_of_pixel=64, grid_width=256, grid_height=256):
+    width  = size_of_pixel * grid_width
+    height = size_of_pixel * grid_height
+    canvas = Image.new("RGB", [width, height], (255,255,255))
+    return canvas
 
 if __name__ == "__main__":
     # create new canvas image
+
+    create_canvas().save("test.jpg")
     pass

a5f4d7df865c5d02cf5c987e5c7c5b947a913f5a
= <psyag8@exmail.nottingham.ac.uk>
2022-02-12 16:41:13 +0000

    not working dominant color

--- src/get_dominant_color.py
+++ src/get_dominant_color.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 from skimage import io
+import imageio
+import convert_to_square as cts
 
 # returns dominant color in an image as RGB values in np array of size 3 [R, G, B]
 # input: path to an image file to profile.
@@ -8,8 +10,11 @@ from skimage import io
 def dom_color(img_path: str) -> np.array:
     """Returns dominant color RGB array for the image provided"""
     img = io.imread(img_path)[:, :, :-1]
-    pixels = np.float32(img.reshape(-1, 3))
-
+    try:
+        pixels = np.float32(img.reshape(-1, 3))
+    except ValueError:
+        pixels = np.float32(img.reshape(-1, 3))
+    #pixels = np.float32(img)
     n_colors = 3
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
     flags = cv2.KMEANS_RANDOM_CENTERS
@@ -19,7 +24,12 @@ def dom_color(img_path: str) -> np.array:
 
     dominant = palette[np.argmax(counts)]
     return dominant
+    #return palette
 
 if __name__ == "__main__":
-    print(dom_color("../images/source/mike.jpg"))
+    img = imageio.read_image("../images/source/red.png")
+    img = cts.make_square(img, 99)
+    imageio.write_image(img, "../images/squared/red.png")
+    print(dom_color("../images/squared/red.png"))
+    print(dom_color("../images/source/red.png"))
     

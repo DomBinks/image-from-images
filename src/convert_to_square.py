24b4e9f2100c7bab770ffd7e897db72b5e6acb33
psydb7 <psydb7@exmail.nottingham.ac.uk>
2022-02-12 13:25:24 +0000

    Working convert_to_square prototype

--- src/convert_to_square.py
+++ src/convert_to_square.py
@@ -6,21 +6,18 @@ if __name__ == "__main__":
     image = cv2.imread('../images/mike.jpg', 1)
     height, width = image.shape[:2]
     print("Height:%s width:%s" % (height, width))
-    
-    #image = image[0:height, (int(width / 2) - int(height / 2)):(int(width / 2) + int(height / 2))]
-    image = image[0:height, 0:width]
 
-    if height > width:
-        if height % 2 == 0:
-            l_offset = height / 2
-            r_offset = height / 2
-        else:
-            l_offset = int(height / 2)
+    if height < width:
+        l_offset = height // 2
+        r_offset = height // 2
+
+        if height % 2 != 0:
             r_offset = int(height / 2) + 1
 
-            image = image[0:height, (width / 2 - height / 2):(width / 2 + height / 2)]
+        image = image[0:height, int((int(width / 2) - l_offset)):int((int(width / 2) + r_offset))]
         
+    cv2.imwrite('../images/squared/mike.jpg', image)
 
     cv2.imshow('img',image)
     cv2.waitKey(0)
-    cv2.destroyAllWindows() 
+    cv2.destroyAllWindows()

7b37c2e5839e1dfa0872b262c32c1c8e8942874a
A1ic3-g <psyag8@exmail.nottingham.ac.uk>
2022-02-12 20:27:19 +0000

    squaring pipeline

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from asyncio.windows_utils import pipe
 import os
 import imageio
 import convert_to_square as cts
@@ -10,17 +9,29 @@ def pipeline():
     os.makedirs("../images/squared", exist_ok=True)
     os.makedirs("../images/final", exist_ok=True)
 
+
+    # Squaring
     files = os.listdir("../images/source")
     i = 0
     for file in files:
         try:
             img = imageio.read_image(f"../images/source/{file}")
+            img = cts.make_square(img, 64)
+            file = file.lower()
+            if "jpg" in file[-3:] or "jpeg" in file[-4:]:
+                ext = "jpg"
+            elif "png" in file[-3:]:
+                ext = "png"
+            else:
+                raise ValueError
+            imageio.write_image(img, f"../images/squared/{i}.{ext}")
+            i+=1
         except FileNotFoundError:
             pass
-        img = cts.make_square(img, 64)
-        
-        i+=1
-
+        except ValueError:
+            pass
+    
+    # Tinting
         
 
 if __name__ == "__main__":

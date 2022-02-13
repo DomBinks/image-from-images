45bdaaf00f4b677f1455a54433dc9e3457ba564a
A1ic3-g <psyag8@exmail.nottingham.ac.uk>
2022-02-13 01:44:15 +0000

    scuffed

--- src/main.py
+++ src/main.py
@@ -5,13 +5,23 @@ import pandas as pd
 import tint
 import get_dominant_color as gdc
 import convert_to_16 as ct16
+import shutil
+import stitching
+
+def cleanup():
+    try:
+        shutil.rmtree("../images/squared")
+        shutil.rmtree("../images/tinted")
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
 
 # data preprocessing
-def pipeline():
+def pipeline(colors):
     os.makedirs("../images/source", exist_ok=True)
     os.makedirs("../images/squared", exist_ok=True)
     os.makedirs("../images/final", exist_ok=True)
-    colors = pd.read_csv("../colors.csv")
+    os.makedirs("../images/reduced", exist_ok=True)
+
     color_keys = colors['key'].to_list()
     
     for color in color_keys:
@@ -61,7 +71,36 @@ def pipeline():
             pass
         except ValueError:
             pass
-        
+
+def select_input_file():
+    files = os.listdir("../images/input")
+    
+    print("Please select a file from the list below as input:")
+    for i in range(len(files)):
+        print(f"    [{i}] {files[i]}")
+    file_no = int(input("File number: "))
+    while file_no < 0 or file_no >= len(files):
+        print("Invalid response please enter a valid file number")
+        file_no = int(input("File number: "))
+    
+    return files[file_no]
+
+def reduce_to_16(file, df_colors):
+    # need to optimise
+    img = imageio.read_image(f"../images/input/{file}")
+    img = cts.make_square(img, 64)
+    imageio.write_image(img, f"../images/reduced/{file}")
+    img = ct16.make_16(img, df_colors)
+    imageio.write_image(img, f"../images/reduced/{file}")
+    
 
 if __name__ == "__main__":
-    pipeline()
+    # can do async
+    input_file = select_input_file()
+    df_colors = pd.read_csv("../colors.csv")
+    cleanup()
+    pipeline(df_colors)
+    reduce_to_16(input_file, df_colors)
+    stitching.stitch(input_file, df_colors, grid_size=64)
+    
+
--- src/stitching.py
+++ src/stitching.py
@@ -1,10 +1,45 @@
 from PIL import Image
+from matplotlib.pyplot import grid
+import imageio
+import random
+import os
 
 def create_canvas(size_of_pixel=64, grid_width=256, grid_height=256):
     width  = size_of_pixel * grid_width
     height = size_of_pixel * grid_height
     canvas = Image.new("RGB", [width, height], (255,255,255))
     return canvas
+
+
+def get_color_key(df_colors, pixel):
+    colors_r = df_colors['r'].to_list()
+    colors_g = df_colors['g'].to_list()
+    colors_b = df_colors['b'].to_list()
+    for i in range(len(colors_b)):
+        if colors_r[i] == pixel[0] and colors_g[i] == pixel[1] and colors_b[i] == pixel[2]:
+            color_row = df_colors.iloc[i]
+            return color_row['key']
+    return "mg"
+
+def get_random_image(key):
+    files = os.listdir(f"../images/tinted/{key}")
+    return random.choice(files)
+
+def stitch(input_file_name, df_colors, pixel_size=64, grid_size=256):
+    canvas = create_canvas(grid_height=grid_size, grid_width=grid_size)
+    input_file = imageio.read_image(f"../images/reduced/{input_file_name}")
+
+    height, width = input_file.shape[:2]
+
+    for row_index in range(height):
+        for col_index in range(width):
+            key = get_color_key(df_colors, input_file[row_index][col_index])
+            pixel_image_name = get_random_image(key)
+            pixel_image = Image.open(f"../images/tinted/{key}/{pixel_image_name}")
+            canvas.paste(pixel_image, [row_index*pixel_size, col_index*pixel_size])
+            
+    canvas.save(f"../images/output/{input_file_name}")
+
 
 if __name__ == "__main__":
     # create new canvas image
